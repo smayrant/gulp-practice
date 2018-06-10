8a1f88b37d7a0cbb6f976fb78a86fced41965fa2
Sheldrick <mrmayrant@yahoo.com>
2018-06-10 02:42:26 -0400

    initial commit of gulp practice

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+gulp.task('styles', function(){
+  console.log('starting styles task');
+})
+
+gulp.task('scripts', function(){
+  console.log('starting scripts task')
+})
+
+gulp.task('images', function (){
+  console.log('starting images task');
+})
+
+gulp.task('default', function(){
+  console.log('default task starting');
+})
